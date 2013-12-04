d36c88bd7758e3fef38d8e4a7c041d8940e498ad
Aaron Murray <aaron@omv-extras.org>
2013-12-03 18:20:18 -0600

    add log plugin

---
+++ var/www/openmediavault/js/omv/module/admin/diagnostic/log/plugin/Greyhole.js
@@ -0,0 +1,62 @@
+/**
+ *
+ * @license   http://www.gnu.org/licenses/gpl.html GPL Version 3
+ * @author    Volker Theile <volker.theile@openmediavault.org>
+ * @copyright Copyright (c) 2009-2013 Volker Theile
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+// require("js/omv/PluginManager.js")
+// require("js/omv/module/admin/diagnostic/log/plugin/Plugin.js")
+// require("js/omv/util/Format.js")
+
+Ext.define("OMV.module.admin.diagnostic.log.plugin.MiniDlna", {
+    extend : "OMV.module.admin.diagnostic.log.plugin.Plugin",
+
+    id       : "greyhole",
+    text     : _("Greyhole"),
+    stateful : true,
+    stateId  : "a4150311-5e3a-4693-8381-933088a9f98f",
+    columns  : [{
+        text      : _("Date & Time"),
+        sortable  : true,
+        dataIndex : "date",
+        id        : "date",
+        renderer  : OMV.util.Format.localeTimeRenderer()
+    },{
+        text      : _("Component"),
+        sortable  : true,
+        dataIndex : "component",
+        id        : "component"
+    },{
+        text      : _("Event"),
+        sortable  : true,
+        dataIndex : "event",
+        id        : "event"
+    }],
+    rpcParams : {
+        id: "greyhole"
+    },
+    rpcFields : [
+        { name : "date", type: "string" },
+        { name : "component", type: "string" },
+        { name : "event", type: "string" }
+    ]
+});
+
+OMV.PluginManager.register({
+    ptype: "diagnostic",
+    id: "log",
+    className: "OMV.module.admin.diagnostic.log.plugin.Greyhole"
+});
